2bc00b84c58ae4115ea33f3ce4dc9cd4e702d9fb
Harsh-Innogent <harsh.patidar@innogent.in>
2024-05-22 15:33:39 +0530

    Initial commit

---
+++ DocumentGeneratorSystem/src/main/java/com/dgs/DocumentGeneratorSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentGeneratorSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentGeneratorSystemApplication.class, args);
+	}
+
+}
---
+++ DocumentGeneratorSystem/src/test/java/com/dgs/DocumentGeneratorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentGeneratorSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
