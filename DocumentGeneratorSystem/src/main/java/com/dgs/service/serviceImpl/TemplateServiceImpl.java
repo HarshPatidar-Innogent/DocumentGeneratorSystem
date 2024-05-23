05586d2b42141af2bc17304c17937d5c5617358c
Harsh-Innogent <harsh.patidar@innogent.in>
2024-05-23 18:43:54 +0530

    Service Interface and Impl

---
+++ DocumentGeneratorSystem/src/main/java/com/dgs/service/serviceImpl/TemplateServiceImpl.java
@@ -0,0 +1,8 @@
+package com.dgs.service.serviceImpl;
+
+import com.dgs.service.iService.ITemplateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemplateServiceImpl implements ITemplateService {
+}
