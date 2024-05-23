d74a35766109f9dc99f74e0ffc0c555fa09a5e87
nihalparate <nihal.parate@innogent.in>
2024-05-23 18:57:44 +0530

    Nihal's interface

--- DocumentGeneratorSystem/src/main/java/com/dgs/service/serviceImpl/DepartmentServiceImpl.java
+++ DocumentGeneratorSystem/src/main/java/com/dgs/service/serviceImpl/DepartmentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.dgs.service.serviceImpl;
 
 import com.dgs.service.iService.IDepartmentService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DepartmentServiceImpl implements IDepartmentService {
 }
