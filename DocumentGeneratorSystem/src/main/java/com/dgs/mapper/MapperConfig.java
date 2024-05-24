1a762da13dd75b066c5620dc333058c18f9650d2
Anisha Gokhru <anisha.gokhru@innogent.in>
2024-05-24 18:01:41 +0530

    Signature Mapper

--- DocumentGeneratorSystem/src/main/java/com/dgs/mapper/MapperConfig.java
+++ DocumentGeneratorSystem/src/main/java/com/dgs/mapper/MapperConfig.java
@@ -2,9 +2,11 @@ package com.dgs.mapper;
 
 import com.dgs.DTO.DocumentDTO;
 import com.dgs.DTO.PlaceholderDTO;
+import com.dgs.DTO.SignatureDTO;
 import com.dgs.DTO.TemplateDTO;
 import com.dgs.entity.Document;
 import com.dgs.entity.Placeholder;
+import com.dgs.entity.Signature;
 import com.dgs.entity.Template;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -43,6 +45,17 @@ public class MapperConfig {
     public Template toTemplate(TemplateDTO templateDTO){
         return mapper.map(templateDTO, Template.class);
     }
+
+    public SignatureDTO toSignatureDTO(Signature signature){
+        return mapper.map(signature, SignatureDTO.class);
+    }
+    public Signature toSignature(SignatureDTO signatureDTO){
+        return mapper.map(signatureDTO, Signature.class);
+    }
+
+
+
+
 
 
 }
